50c9fef8d375cf77ca412d79c40065f59f72fe99
Steven Grimm <143267939+StevenGrimm@users.noreply.github.com>
2023-10-13 13:31:08 +0000

    updated with grade %

--- src/class_roster_lib.cpp
+++ src/class_roster_lib.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <limits>
 #include <tuple>
-#include "class_roster.h" 
+#include "class_roster.h"
 
 namespace ctch1330
 {
@@ -123,6 +123,40 @@ void DisplayGrade(vector<string> roster, vector<string> grade)
 		
 		return user_selection;
 	}
+
+	int main(){
+    int marks;
+    cout<<"Enter Your Marks: ";
+    cin>>marks;
+    if (marks >= 90){
+        cout<<"Your Grade is A+";
+    }
+    else if (marks >= 80){
+        cout<<"Your Grade is A";
+    }
+    else if (marks >= 70){
+        cout<<"Your Grade is B+";
+    }
+    else if (marks >= 60){
+        cout<<"Your Grade is B";
+    }
+    else if (marks >= 50){
+        cout<<"Your Grade is C";
+    }
+    else if (marks >= 40){
+        cout<<"Your Grade is D";
+    }
+    else if (marks >= 30){
+        cout<<"Your Grade is E";
+    }
+    else if (marks <= 30){
+        cout<<"Your Grade is F";
+    }
+    else{
+        cout<<"Enter Valid Marks";
+    }
+    return 0;
+}
 
 	void DispatchSelectedOperation(int user_selection)
 	{
